5925d3bef645f07883fb895012b236480363c847
Sergey Kulikov <inffinity1@gmail.com>
2022-10-11 04:22:13 +0300

    ss

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,10 @@
 export function About() {
   return (
     <>
-      <h1>About Page</h1>
+      <h1>О нас</h1>
 
       <p>
-        This project was created using React and Bootstrap 5. The banner on the
-        homepage will generate a random item from the shop everytime querying
-        the JSON data. The shopping cart is accessible from the whole site using
-        React Context. Items added to the cart are stored in local storage so
-        even if you refresh the page, they are still there. The cart will
-        calculate the total price and allow to to remove items from it. The
-        store page will also allow you to add and remove items from the cart.
-        Overall this was a really fun project and great learning experience.
-        Signing out, Nav
+        Небольшой интернет магазин видеоигр. Номер телефона 8800-555-35-35 проще позвонить, чем у кого-то занимать.
       </p>
     </>
   );
