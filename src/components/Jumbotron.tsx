714485d98254dcc3d02a1e15e268a81aa2204427
Sergey Kulikov <inffinity1@gmail.com>
2022-10-15 11:04:52 +0300

    some

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -1,21 +1,19 @@
 export function Jumbotron() {
   return (
     <>
-
-        <div className="landing-wrapper container p-5">
-          <h1 className="display-4">Добро пожаловать в мой магазин игр Pokemon</h1>
-          <hr />
-          <p>В магазин</p>
-          <a href="/store" className="btn btn-primary">
-            Купи сейчас!
-          </a>
-          <hr />
-          <div className="pokemonbg"></div>
-          <h3>
-            Этот магазин крутой!
-          </h3>
-        </div>
-
+      <div className="landing-wrapper container p-5">
+        <h1 className="display-4">
+          Добро пожаловать в мой магазин игр GameStore
+        </h1>
+        <hr />
+        <p>В магазин</p>
+        <a href="/store" className="btn btn-primary">
+          Купи сейчас!
+        </a>
+        <hr />
+        <div className="pokemonbg"></div>
+        <h3>Магазин видеоигр для игровых систем и персональных компьютеров.</h3>
+      </div>
     </>
   );
 }
